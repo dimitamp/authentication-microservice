a92a66425a6382dc9f28eb623d0be7fda3e927bb
Dimitris Tampakis <dimitamp@auth.gr>
2019-07-09 11:24:57 +0300

    Cors Issue

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -20,7 +20,6 @@ const cors = handler => (req, res, ...restArgs) => {
   res.setHeader('access-control-allow-origin', '*');
   res.setHeader('access-control-allow-methods', allowMethods.join(','));
   res.setHeader('access-control-allow-headers', allowHeaders.join(','));
-  console.log(req.headers);
   console.log(res.headers);
   return handler(req, res, ...restArgs);
 };
