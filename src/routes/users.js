790bedd0eb714d577874b9de689c5fb710b2b0ec
unknown <dimitamp@auth.gr>
2019-07-13 12:11:29 +0300

    Changed line endings

--- src/routes/users.js
+++ src/routes/users.js
@@ -101,7 +101,6 @@ router.post(
           message: 'Registration Error: A user with this e-mail  already exists.'
         });
       }
-      const mpampisaasasghhgs;
       const newUser = await new User({
         email,
         password,
