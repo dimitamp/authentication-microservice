5121649ac4e6da8d512756a23dcbc30d716bcc1b
unknown <dimitamp@auth.gr>
2019-07-13 11:57:12 +0300

    Api Documentation

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,6 +7,87 @@ const router = express.Router();
 
 const User = require('../models/user');
 
+
+/**
+ * @api {post} /users/ Creates a new User
+ * @apiName PostUsers
+ * @apiGroup User
+ * @apiVersion 1.0.0
+ *
+ * @apiParam {String} email Required email
+ * @apiParam {String{8..}} password Required password
+ * @apiParam {String="role1","role2","role3"} role Required role 
+ * @apiParamExample {json} Request-example: 
+ *     {
+ *        "email": "test@email.com", 
+ *        "password": "01234567",
+ *        "role": "role1"
+ *     }
+ * 
+ * @apiSuccess {String} token Authorization token.
+ * @apiSuccess {Object} user User information.
+ * @apiSuccess {String} user.email User's email.
+ * @apiSuccess {String} user.role User' role.
+ * @apiSuccess {String} user.id User's id.
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       user: {
+ *           "id": "5d0bd41c2b85baa50300002d",
+ *           "email": "test@email.com",
+ *           "role": "role1"
+ *       },
+ *       token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWQwYmQ0MWMyYjg1YmFhNTAzMDAw
+ *               MDJkIiwiZW1haWwiOiJmb25pa2hteWdhQGdtYWlsLmNvbSIsImlhdCI6MTU2MjEwMjc4N30.I64zY_Fj-
+ *               D30vttEDMaQBPaheuTLGX1F1Ap5pynpOBs"
+ *     }
+ *
+ * @apiError UserAlreadyExists A user with this e-mail already exists.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 409 Resource Conflict
+ *     {
+ *       "status": 409
+ *       "message": "Registration Error: A user with this e-mail  already exists."
+ *     }
+ * 
+ * @apiError MissingEmail Missing required parameter e-mail.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": 400
+ *       "message": "Validation Error: E-mail is required."
+ *     }
+ * 
+ * @apiError InvalidEmail Invalid required parameter e-mail.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": 400
+ *       "message": "Validation Error: E-mail is invalid."
+ *     }
+ * 
+ * @apiError MissingPassword Missing required parameter password.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": 400
+ *       "message": "Validation Error: Password is required."
+ *     }
+ * 
+ * @apiError InvalidPassword Invalid required parameter e-mail.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": 400
+ *       "message": "Validation Error: Password must have at least 8 characters."
+ *     }
+ */
 router.post(
   '/',
   (req, res, next) => validation(req, res, next, 'register'),
@@ -34,6 +115,93 @@ router.post(
     }
   }
 );
+
+/**
+ * @api {post} /users/authenticate Authenticates a user
+ * @apiName PostUsersAuthenticate
+ * @apiVersion 1.0.0
+ * @apiGroup User
+ *
+ * @apiParam {String} email Required email
+ * @apiParam {String{8..}} password Required password
+ * @apiParamExample {json} Request-example: 
+ *     {
+ *        "email": "test@email.com", 
+ *        "password": "01234567"
+ *     }
+ * 
+ * @apiSuccess {String} token Authorization token.
+ * @apiSuccess {Object} user User information.
+ * @apiSuccess {String} user.email User's email.
+ * @apiSuccess {String} user.role User' role.
+ * @apiSuccess {String} user.id User's id.
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       user: {
+ *           "id": "5d0bd41c2b85baa50300002d",
+ *           "email": "test@email.com",
+ *           "role": "role1"
+ *       },
+ *       token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWQwYmQ0MWMyYjg1YmFhNTAzMDAw
+ *               MDJkIiwiZW1haWwiOiJmb25pa2hteWdhQGdtYWlsLmNvbSIsImlhdCI6MTU2MjEwMjc4N30.I64zY_Fj-
+ *               D30vttEDMaQBPaheuTLGX1F1Ap5pynpOBs"
+ *     }
+ *
+ * @apiError UserDoesntExist A user with this e-mail not found.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 401 Authentication error
+ *     {
+ *       "status": 401
+ *       "message": "Authentication Error: A user with this e-mail  not found."
+ *     }
+ * 
+ * @apiError InvalidCredentials Password doesn't match with given e-mail.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 401 Authentication error
+ *     {
+ *       "status": 401
+ *       "message": "Authentication Error: Password doesn't match with given e-mail."
+ *     }
+  * @apiError MissingEmail Missing required parameter e-mail.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": 400
+ *       "message": "Validation Error: E-mail is required."
+ *     }
+ * 
+ * @apiError InvalidEmail Invalid required parameter e-mail.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": 400
+ *       "message": "Validation Error: E-mail is invalid."
+ *     }
+ * 
+ * @apiError MissingPassword Missing required parameter password.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": 400
+ *       "message": "Validation Error: Password is required."
+ *     }
+ * 
+ * @apiError InvalidPassword Invalid required parameter e-mail.
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "status": 400
+ *       "message": "Validation Error: Password must have at least 8 characters."
+ *     }
+ */
 
 router.post(
   '/authenticate',
